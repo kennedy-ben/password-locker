84caed800e2ff53e178f5af7d0b18d994426185f
kennedy bernard <makuanikennedy@gmail.com>
2022-01-21 11:00:16 +0300

    credentials

---
+++ credential.py
@@ -0,0 +1,34 @@
+import string
+import random
+
+class credentials:
+    pass
+    class that generates new user and their credentials
+    pass
+    credentials list = [] # credential list
+
+    @classmethord
+    def check_user(user_name,password):
+        pass
+        methord that is used to check if the credentilas entered matches entry in the user_list
+        pass 
+        for user in users.user-list:
+            if (user.user_name == user_name and user.password == password):
+                current_user = user.user_name
+                return current_user
+      def __init__(self, user_name, account, account_username, password):
+          pass
+          __init__methord  that helps to define our propeties from our credential
+          pass
+          self.user_name = user_name
+          self.account = account
+          self.account_username = account_username
+          self.password = password
+     def save_credentials(self):
+         pass
+         save alredy printed credential
+         pass
+     credentials.credential_list.append(self)
+
+     def generate_password(size=10, )    
+
